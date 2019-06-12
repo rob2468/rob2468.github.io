38755076612cad635338dc89075f5c618b946d83
jam.chenjun <jam.chenjun@gmail.com>
2019-06-12 23:24:38 +0800

    feat: 访问量统计，支持显示柱形图和饼图

--- pages/statistic/index.js
+++ pages/statistic/index.js
@@ -17,116 +17,138 @@ async function init() {
     // 结果解析
     const result = responseJSON.result;
 
-    const yAxis = [];
+    // 显示总览图
+    renderSummaryChart(now, result);
 
-    // 所有查询的日期
-    const displayTimeArr = [];
-    let timestamp = now; // 初始时间戳
-    let i = 0;
-    while (i < days) {
-      // 计算出所有查询的日期
-      const dateStr = getFormattedBeijingDateString(timestamp);
-      displayTimeArr.unshift(dateStr);
+    // 显示当天的访问比例图
+    renderDailySectorChart(displayTime, result[displayTime]);
+  }
+}
 
-      // y 轴值
-      let yVal = 0;
-      const dayData = result[dateStr];
-      dayData.forEach(ele => {
-        const {
-          count = 0,
-          pageId = '',
-          title = '',
-        } = ele;
-        yVal += count;
-      });
-      yAxis.unshift(yVal);
+/**
+ * 柱状图
+ * @param {number} now timestamp
+ * @param {object} sourceData 原始数据
+ */
+function renderSummaryChart(now, sourceData) {
+  // 构造数据
+  const xAxis = [];
+  const yAxis = [];
+  let timestamp = now; // 初始时间戳
+  let i = 0;
+  const days = Object.keys(sourceData).length;
+  while (i < days) {
+    // 计算出所有查询的日期
+    const dateStr = getFormattedBeijingDateString(timestamp);
+    xAxis.unshift(dateStr);
 
-      // 前一天
-      timestamp -= 24 * 60 * 60 * 1000;
-      i++;
-    }
+    // y 轴值
+    let yVal = 0;
+    const dayData = sourceData[dateStr];
+    dayData.forEach(ele => {
+      const {
+        count = 0
+      } = ele;
+      yVal += count;
+    });
+    yAxis.unshift(yVal);
 
-    const xAxis = displayTimeArr;
-    renderChart(xAxis, yAxis);
+    // 前一天
+    timestamp -= 24 * 60 * 60 * 1000;
+    i++;
   }
 
-
-
-  // const now = new Date(); // 从当天开始，向前请求若干天的数据
-  // const xAxis = [];
-  // const yAxis = [];
-  // const length = 10;  // 横坐标值的数量
-  // let num = 0;
-  // buildChart(now, num, length, xAxis, yAxis);
-}
-
-function buildChart(date, num, length, xAxis, yAxis) {
-  if (num >= length) {
-    // 达到指定数量，渲染
-    renderChart(xAxis, yAxis);
-    return;
-  }
-
-  // 请求数据
-  requestData(date, (result) => {
-    const dateString = getFormattedDateString(date);
-    xAxis.unshift(dateString);
-
-    if (result.success) {
-      yAxis.unshift(result.data.count);
-    } else {
-      yAxis.unshift(0);
-    }
-
-    const millisecondsADay = 24 * 60 * 60 * 1000;
-    date = new Date(date.getTime() - millisecondsADay);
-    num++;
-    buildChart(date, num, length, xAxis, yAxis);
-  });
-}
-
-function requestData(date, callback) {
-  const dateString = getFormattedDateString(date);
-  const cql = `select count(*) from counter where createDate = "${dateString}"`;
-
-  AV.Query.doCloudQuery(cql).then(function (data) {
-    callback({
-      success: true,
-      data,
-    });
-  }, function (error) {
-    callback({
-      success: false,
-    });
-  });
-}
-
-function renderChart(xAxis, yAxis) {
  // 基于准备好的dom，初始化echarts实例
-  var myChart = echarts.init(document.getElementById('summary'));
+  const myChart = echarts.init(document.getElementById('summary'));
 
   // 指定图表的配置项和数据
-  var option = {
+  const option = {
     title: {
       text: 'pv总览',
     },
-    tooltip: {},
+    tooltip: {
+      trigger: 'axis',
+      axisPointer : {
+        type : 'shadow',
+      }
+    },
     legend: {
-        data: [],
+      data: [],
     },
     xAxis: {
-        data: xAxis,
+      data: xAxis,
     },
     yAxis: {},
     series: [{
-      name: '',
+      name: 'pv',
       type: 'bar',
       data: yAxis,
-    }, {
-      name:'',
-      type:'line',
-      data: yAxis,
     }],
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+
+  // 响应点击事件
+  myChart.on('click', param => {
+    if (param.name) {
+      renderDailySectorChart(param.name, sourceData[param.name]);
+    }
+  });
+}
+
+/**
+ * 饼图
+ * @param {string} displayTime
+ * @param {array} dayData
+ */
+function renderDailySectorChart(displayTime, dayData) {
+  // 构造数据
+  const data = dayData.map(currentValue => {
+    const {
+      count = 0,
+      title = '',
+    } = currentValue;
+    return {
+      name: title,
+      value: count,
+    };
+  });
+
+  // 基于准备好的dom，初始化echarts实例
+  const myChart = echarts.init(document.getElementById('daily-sector'));
+
+  // 指定图表的配置项和数据
+  const option = {
+    title : {
+      text: `${displayTime}`,
+    },
+    tooltip : {
+      trigger: 'item',
+      formatter: "{b} : {c} ({d}%)"
+    },
+    legend: {
+      type: 'scroll',
+      orient: 'vertical',
+      right: 10,
+      top: 20,
+      bottom: 20,
+    },
+    series : [{
+        name: '',
+        type: 'pie',
+        radius : '80%',
+        center: ['50%', '50%'],
+        data,
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+          }
+        }
+      }
+    ]
   };
 
   // 使用刚指定的配置项和数据显示图表。
